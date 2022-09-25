0b1698d68ca91fbe6fb3406f351238f85df98e8d
이원재 <ysm04038@gmail.com>
2022-09-25 14:33:16 +0900

    양도예정일 elevated button을 textfeild로 변경

--- lib/widgets/custom_datepicker.dart
+++ lib/widgets/custom_datepicker.dart
@@ -9,25 +9,47 @@ class CustomDatePicker extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final controller = Get.find<CapitalGainsParameter>();
+    String example = '날짜를 입력해 주세요.';
+
     return Obx(() {
       print(controller.param.value);
-      // 외부의 Obx와 별개로 내부에서 Obx를 한번 더 호출하는 이유는 드랍다운 값 선택 시 해당 값이 드롭다운에 표기되도록 렌더링하기 위함입니다(setState() 역할)
-      return ElevatedButton(
-          child: Text("Pick date"),
-          onPressed: () {
-            Future<DateTime?> future = showDatePicker(
-                context: context,
-                initialDate: DateTime.now(),
-                firstDate: DateTime(1970),
-                lastDate: DateTime(2100),
-                builder: (BuildContext context, Widget? child) {
-                  return Theme(data: ThemeData.light(), child: child!);
-                });
 
-            future.then((date) {
-              controller.setParam(keyValue, date);
-            });
-          });
+      // 외부의 Obx와 별개로 내부에서 Obx를 한번 더 호출하는 이유는 드랍다운 값 선택 시 해당 값이 드롭다운에 표기되도록 렌더링하기 위함입니다(setState() 역할)
+      return Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Text(keyValue),
+          GestureDetector(
+            child: Container(
+              margin: EdgeInsets.fromLTRB(0, 10, 0, 10),
+              decoration: const BoxDecoration(
+                  border: Border(
+                      bottom: BorderSide(
+                          color: Colors.black26
+                      )
+                  )
+              ),
+              child: Text(example),
+            ),
+
+            onTap: (){
+              Future<DateTime?> future = showDatePicker(
+                  context: context,
+                  initialDate: DateTime.now(),
+                  firstDate: DateTime(1970),
+                  lastDate: DateTime(2100),
+                  builder: (BuildContext context, Widget? child) {
+                    return Theme(data: ThemeData.light(), child: child!);
+                  });
+
+              future.then((date) {
+                controller.setParam(keyValue, date);
+                example = '${date!.year}-${date!.month}-${date!.day}';
+              });
+            },
+          ),
+        ],
+      );
     });
   }
 }
